db78ea80ec7ca634373f9115a035dbf8c15091b6
channingwalton <channingwalton@55182dad-1612-0410-990f-f40f6c36956b>
2006-08-06 08:55:39 +0000

    added support for embedding Wiki documents

---
+++ trunk/eclipsewiki/test/com/teaminabox/eclipse/wiki/text/EmbeddedWikiWordMatcherTest.java
@@ -0,0 +1,18 @@
+package com.teaminabox.eclipse.wiki.text;
+
+public class EmbeddedWikiWordMatcherTest extends AbstractTextRegionMatcherTest {
+
+	protected TextRegionMatcher getMatcher() {
+		return new EmbeddedWikiWordMatcher(new WikiWordMatcher("([A-Z][a-z]+){2,}"));
+	}
+
+	protected TextRegionTestBean[] getAcceptableCases() {
+		return new TextRegionTestBean[] { 
+				new TextRegionTestBean("Embed:WikiWord", new EmbeddedWikiWordTextRegion("Embed:WikiWord", new WikiWordTextRegion("WikiWord"))),
+				new TextRegionTestBean("Embed:WikiWord stuff", new EmbeddedWikiWordTextRegion("Embed:WikiWord", new WikiWordTextRegion("WikiWord")))};
+	}
+
+	protected String[] getUnacceptableText() {
+		return new String[] {"WikiWord", "word", "123", "WikiWord followed by stuff", "Embed:"};
+	}
+}
