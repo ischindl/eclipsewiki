812c8bb13ae67f072f7504a0ca0210db7e12bbdc
channingwalton <channingwalton@55182dad-1612-0410-990f-f40f6c36956b>
2007-03-02 23:42:05 +0000

    cleaned up

--- eclipsewiki/src/com/teaminabox/eclipse/wiki/util/Strings.java
+++ eclipsewiki/src/com/teaminabox/eclipse/wiki/util/Strings.java
@@ -16,4 +16,20 @@ public final class Strings {
 		}
 		return buffer.toString();
 	}
+
+	public static boolean isWhiteSpaceCharacter(char c) {
+		return c == ' ' || c == '\t' || c == '\n' || c == '\r';
+	}
+
+	public static int indexOfWhiteSpace(String text) {
+		if (text.length() == 0) {
+			return -1;
+		}
+		for (int i = 0; i < text.length(); i++) {
+			if (Character.isWhitespace(text.charAt(i))) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
