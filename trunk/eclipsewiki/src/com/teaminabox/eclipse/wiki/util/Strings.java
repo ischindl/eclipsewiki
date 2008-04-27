6ef3ea06f40708993c612747c14dc7320d4b76bd
channingwalton <channingwalton@55182dad-1612-0410-990f-f40f6c36956b>
2008-04-27 08:29:58 +0000

    refactored

--- trunk/eclipsewiki/src/com/teaminabox/eclipse/wiki/util/Strings.java
+++ trunk/eclipsewiki/src/com/teaminabox/eclipse/wiki/util/Strings.java
@@ -6,15 +6,7 @@ public final class Strings {
 	 * Put spaces before the capital of a wiki word (except the first character).
 	 */
 	public static String deCamelCase(String wikiWord) {
-		StringBuffer buffer = new StringBuffer(wikiWord.length() * 2);
-		buffer.append(wikiWord.charAt(0));
-		for (int i = 1; i < wikiWord.length(); i++) {
-			if (!Character.isWhitespace(wikiWord.charAt(i - 1)) && Character.isUpperCase(wikiWord.charAt(i))) {
-				buffer.append(' ');
-			}
-			buffer.append(wikiWord.charAt(i));
-		}
-		return buffer.toString();
+		return wikiWord.replaceAll("([A-Z])", " $0").trim();
 	}
 
 	public static boolean isWhiteSpaceCharacter(char c) {
