1ddf23506dbbacc6088582bde0d1fa614861a462
channingwalton <channingwalton@55182dad-1612-0410-990f-f40f6c36956b>
2010-08-31 20:23:45 +0000

    added the patch provided by Keramidas Stratos

--- trunk/eclipsewiki/src/com/teaminabox/eclipse/wiki/editors/WikiScanner.java
+++ trunk/eclipsewiki/src/com/teaminabox/eclipse/wiki/editors/WikiScanner.java
@@ -11,15 +11,14 @@ import com.teaminabox.eclipse.wiki.text.TextRegionMatcher;
 
 public class WikiScanner extends RuleBasedScanner {
 
-	@SuppressWarnings("unchecked")
 	public WikiScanner(WikiEditor wikiEditor) {
-		ArrayList rules = new ArrayList();
+		ArrayList<IRule> rules = new ArrayList<IRule>();
 		rules.add(new WhitespaceRule(new WikiWhitespaceDetector()));
 		TextRegionMatcher[] matchers = wikiEditor.getContext().getContentRenderer().getScannerMatchers();
 		for (TextRegionMatcher element : matchers) {
 			element.setEditor(wikiEditor);
 		}
 		rules.addAll(Arrays.asList(matchers));
-		setRules((IRule[]) rules.toArray(new IRule[rules.size()]));
+		setRules(rules.toArray(new IRule[rules.size()]));
 	}
 }
